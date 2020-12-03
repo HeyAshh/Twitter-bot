e4d097085e5825d9ba906ecb758d4ad16341b92f
Ashh <Heyashh1@gmail.com>
2020-12-03 20:49:27 +0300

    Update sheet.js

--- sheet.js
+++ sheet.js
@@ -1,1 +1,15 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
+
+module.exports = class Sheet {
+    constructor() {
+        this.doc = new GoogleSpreadsheet('************************');
+    }
+    async load() {
+        await this.doc.useServiceAccountAuth(require('./credentials.json'));
+        await this.doc.loadInfo();
+    }
+    async addRows(rows) { 
+        const sheet = this.doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
+        await sheet.addRows(rows);
+    }
+}
